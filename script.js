9339a282cb6c62988d89d416e134424ae83f0135
Dev_2o_Ano <quezia.p.silva6@aluno.senai.br>
2026-03-06 11:42:39 -0300

    Etapa 1 concluída:
    Menu mobile funcional.

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 // 1ª Digitação (JS Completo)
-const menuIcon = document.querySelector('#menu-icon')
+const menuIcon = document.querySelector('#menu-icon');
+const navList = document.querySelector('.navlist');
+
+menuIcon.addEventListener('click', () => {
+  menuIcon.classList.toggle('bx-x');
+  navList.classList.toggle('open');
+
+  // Bloquear scroll quando menu aberto 
+  document.body.style.overflow = navList.classList.contains('open') ? 'hidden' : 'auto'
+});
+
